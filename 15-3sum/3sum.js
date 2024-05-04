1f1c3764337afc385df570b289e12a199eaf7968
Anshuman Upadhyay <anshumanupy@gmail.com>
2024-05-04 18:07:25 +0530

    Time: 327 ms (17.12%) | Memory: 65 MB (69.60%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,50 +2,46 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+ // nums[i] = -(nums[j] + nums[k])
 var threeSum = function(nums) {
-    let triplets = new Map(); // Store sorted triplets as keys and do not add if same.
-    nums.sort((a,b) => a-b);
-
+    triplets = new Map();
+    nums = nums.sort((a, b) => a - b);
     for(let i=0; i<=nums.length-3; i++) {
-        let j = i+1;
-        let k = nums.length - 1;
         let visitedJ = undefined;
         let visitedK = undefined;
-        while(j<k) {
-            if(visitedJ !== undefined && nums[j] == visitedJ) {
+        let j = i+1;
+        let k = nums.length - 1;
+
+        while(j < k) {
+            if(visitedJ == nums[j]) {
                 j++;
                 continue;
-            }
-            if(visitedK !== undefined && nums[k] == visitedJ) {
-                k--;
-                continue;
-            }
-            visitedJ = undefined;
-            visitedK = undefined;
-            const sum = nums[i]+nums[j]+nums[k];
-            if(sum === 0) {
-                let ar = [nums[i], nums[j], nums[k]];
-                // ar.sort((a, b) => a-b); // You don't need to sort it again!
-                if(!triplets.has(ar.join("|"))) {
-                    triplets.set(ar.join("|"), true);
-                }
-              visitedJ = nums[j];
-              visitedK = nums[k];
-            } else if(sum > 0) {
-                k--;
-            } else if(sum < 0) {
-                j++;
             }
 
-            // j++;
-            // k--;
+            if(visitedK == nums[k]) {
+                k--;
+                continue;
+            }
+
+            const sum = nums[i] + nums[j] + nums[k];
+            if(sum === 0) {
+                visitedJ = nums[j];
+                visitedK = nums[k];
+                triplets.set(`${nums[i]}|${nums[j]}|${nums[k]}`, true);
+            } else if(sum > 0) {
+                k--;
+            } else {
+                j++;
+            }
         }
     }
 
-    let result = [];
-    for(const [key, value] of triplets) {
-        result.push(key.split("|").map(val => +val))
+    let output = [];
+
+    for(let key of triplets.keys()) {
+        output.push(key.split("|").map(val => +val));
     }
 
-    return result;
+    return output;
+
 };
