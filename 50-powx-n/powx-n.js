d066159f0cf3348cac6c024465cf7092ff5b9046
Anshuman Upadhyay <anshumanupy@gmail.com>
2024-06-29 18:30:10 +0530

    Time: 54 ms (55.24%) | Memory: 48.2 MB (95.57%) - LeetSync

--- 50-powx-n/powx-n.js
+++ 50-powx-n/powx-n.js
@@ -3,21 +3,25 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
-    if(n==0) return 1;
-    let negative = n < 0 ? true : false;
-    n = Math.abs(n);
-    let m = 1;
-    while(n > 0) {
-        if(n%2 == 0) {
-            x = (x*x);
-            n = n/2;
-        } else {
-            m *= x;
-            n -= 1;
-        }
+var myPow = function(x, n, mul = 1, negative=false) {
+    if(n === 0) return 1;
+    if(n < 0) {
+        negative = true;
+        n *= -1;
     }
+    if(n <= 1) {
+        mul *= x;
+        if(negative) return 1/mul;
+        return mul;
+    } 
 
-    if(negative) return 1/m;
-    return m;
+    if(n % 2 === 0) {
+        x = (x * x);
+        n = n / 2;
+        return myPow(x, n, mul, negative);
+    } 
+
+    mul = mul * x;
+    n = n - 1;
+    return myPow(x, n, mul, negative);
 };
