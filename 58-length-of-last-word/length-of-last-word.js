cb6bc75d29d6d7cb7f0c4c2ceed1a768cdf118bf
Anshuman Upadhyay <hello@anshumanupadhyay.me>
2025-03-26 07:50:36 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (16.62%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let i = s.length - 1;
+    let count = 0;
+    while(s[i] == " " && i >= 0) {
+        i--;
+    }
+
+    while(s[i] != " " && i >= 0) {
+        count++;
+        i--;
+    }
+
+    return count;
+};
