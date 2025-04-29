3310f869892d94b7fcd8fadc302b1aefb68f017b
Anshuman Upadhyay <hello@anshumanupadhyay.me>
2025-04-29 08:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (38.99%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -3,16 +3,33 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    let n = matrix.length;
-    for(let i=0; i<n; i++) {
-        for(let j=i; j<n; j++) {
-            let tempRow = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = tempRow;
-        }
-    } 
+    // 1. Transpose the matrix in-place
+    // 2. Reverse elements in every row
 
-    for(let i=0; i<n; i++) {
-       matrix[i] = matrix[i].reverse();
-    } 
+    for(let i=0; i < matrix.length; i++) {
+        for(let j=i; j < matrix[i].length; j++) {
+            const sum = matrix[j][i] + matrix[i][j];
+            matrix[j][i] = sum - matrix[j][i];
+            matrix[i][j] = sum - matrix[i][j];
+        }
+    }
+
+    let i = 0;
+    let k = 0;
+    let j = matrix[k].length - 1;
+    while(i < matrix.length) {
+        while( k < j ) {
+            const sum = matrix[i][k] + matrix[i][j];
+            matrix[i][k] = sum - matrix[i][k];
+            matrix[i][j] = sum - matrix[i][j];
+            k++;
+            j--;
+        }
+
+        i++;
+        if(i < matrix.length) {
+            k = 0;
+            j = matrix[i].length - 1;
+        }
+    }
 };
