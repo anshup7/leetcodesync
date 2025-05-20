a88da2dfa298bc7b4264f7fa3bd506ed5bfc1cbf
Anshuman Upadhyay <hello@anshumanupadhyay.me>
2025-05-20 23:55:13 +0530

    Time: 4 ms (54.95%) | Memory: 55.2 MB (41.30%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -7,16 +7,11 @@ var twoSum = function(nums, target) {
     let start = 0;
     const mapper = new Map();
     for(let i = 0; i < nums.length; i++) {
-        if(!mapper.has(nums[i])) {
-            mapper.set(nums[i], i);
-        }
-    }
-
-    for(let i=0; i < nums.length; i++) {
         const residue = target - nums[i];
         if(mapper.has(residue)) {
             const index = mapper.get(residue);
-            if(index !== i) return [i, mapper.get(residue)];
+            return [mapper.get(residue), i];
         }
+        mapper.set(nums[i], i);
     }
 };
